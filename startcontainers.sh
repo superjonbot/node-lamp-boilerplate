ccbc388895cb13824f194cb981aa4bb3b00ffa25
Jonathan Robles <delsoul@gmail.com>
2017-07-27 10:47:20 -0400

    fixed symbolic link

--- startcontainers.sh
+++ startcontainers.sh
@@ -3,5 +3,7 @@ docker run --name wordpressdb -v $(pwd)/SOURCE/sql:/tmp -e MYSQL_ROOT_PASSWORD=p
 docker run -p 8080:80 -e WORDPRESS_DB_PASSWORD=password -d --name wordpress -v $(pwd)/SOURCE/html:/var/www/html --link wordpressdb:mysql  aenetworks/wordpress:1.0
 
 # NODE
+rm /Users/superjonbot/Sites/AE/api.ai.docker/SOURCE/node
+ln -s /Users/superjonbot/Sites/AE/api.ai.docker/node_docker/node /Users/superjonbot/Sites/AE/api.ai.docker/SOURCE
 cd node_docker
 docker run -it -p 3000:3000 -v $(pwd)/../SOURCE/node:/usr/src/app aenetworks/node:1.0
